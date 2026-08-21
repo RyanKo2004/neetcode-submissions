b98ecfdf34c358360b933eeffb2e2df82157f7f3
RyanKo2004 <167986894+RyanKo2004@users.noreply.github.com>
2026-08-22 11:00:43 +1200

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int pointer1 = 0;
+        int pointer2 = numbers.length-1;
+        
+
+        while(pointer1 < pointer2){
+            if(numbers[pointer1]+numbers[pointer2] == target){
+                return new int[]{pointer1+1,pointer2+1};
+            }
+            if(numbers[pointer1]+numbers[pointer2] > target){
+                pointer2--;
+            }
+            if(numbers[pointer1]+numbers[pointer2] < target){
+                pointer1++;
+            }
+        }
+        return null;
+
+    }
+}
